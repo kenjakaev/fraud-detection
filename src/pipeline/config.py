a467004fb8273cb9bf7463f7227a793643524d9f
kenjakaev <arsen.kenjakaev.dev@gmail.com>
2026-08-10 15:42:39 +0500

    feat: tested model on new data and achieved AUC score of 0.96334

--- src/pipeline/config.py
+++ src/pipeline/config.py
@@ -8,7 +8,8 @@ PROCESSED_DATA_DIR = BASE_DIR / "data" / "processed"
 TRAIN_TRANSACTION_PATH = RAW_DATA_DIR / "train_transaction.csv"
 TRAIN_IDENTITY_PATH = RAW_DATA_DIR / "train_identity.csv"
 
-TRAIN_DF_PATH = PROCESSED_DATA_DIR / "train_merged.parquet"
+TRAIN_MERGED_PATH = PROCESSED_DATA_DIR / "train_merged.parquet"
+TRAIN_DF_PATH = PROCESSED_DATA_DIR / "final_train_df.parquet"
 
 RANDOM_SEED = 42
 VAL_SIZE = 0.2
